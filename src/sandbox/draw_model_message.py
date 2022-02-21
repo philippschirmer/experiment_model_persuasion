c8b7aae564b50f9ce39b174defcac086a9e2d756
philippschirmer <schirmerphilipp@gmail.com>
2022-02-22 00:36:55 +0100

    First Plotly graphs, wordstrings for Python functions to be written

---
+++ src/sandbox/draw_model_message.py
@@ -0,0 +1,21 @@
+# TODO: Function that "draws" a specified model message into data
+
+
+def draw_model_message(data, model, hyperparams):
+    ''' TODO: function takes as input prespecified data and model to be drawn into it.
+        As an output, generates a picture file and saves it to prespecified location.
+
+        Inputs:
+        data:  2xT (2D) np.array, containing dependent and independent variable.
+        model: integer  - model message to be sent.
+        hyperparams (optional?) - hyperparameters to be used in generating the picture.
+
+        Outputs:
+
+        message_`data'_`model'.png - model message as picture file.
+
+        TODO: Save directly as part of the function? Then no real output
+    
+    
+    '''
+    pass
---
+++ src/sandbox/randomly_draw_model.py
@@ -0,0 +1,12 @@
+# TODO: function that based on model space and model prior on that space, draws a realized model
+# could be just a random choice function?
+
+def randomly_draw_model(model_space, model_prior):
+    '''
+    Input: Model_space 1D array
+        model prior: 1D array, probability distribution w/ standard attributes.
+
+
+    Output: realized model choice (scalar?)
+    '''
+    pass
